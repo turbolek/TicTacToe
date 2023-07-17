0aaa4a66af33588072e2164e19059583e1e7b91d
turbolek <dankunicki@gmail.com>
2023-07-17 23:26:02 +0200

    Spawning board of a given size

---
+++ TicTacToe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private BoardSpawner _boardSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _boardSpawner.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
